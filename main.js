ae24851ffa800b5f1bed1a5a8663afd81eb272aa
Shourya137 <75378252+Shourya137@users.noreply.github.com>
2021-07-23 10:02:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+song="";
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+   
+    video=createCapture(VIDEO);
+    video.hide();
+
+   poseNet=ml5.poseNet(video,modelLoaded);
+  
+   }
+   
+
+   function draw()
+{
+    image(video,0,0,600,500);
+}
+ 
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function play(){
+    song.play();
+    
+}
